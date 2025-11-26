24c00af90fc32b26d050525addcdaf7ca06e9395
Tianhao-Gu <tgu@anl.gov>
2025-11-26 13:44:18 -0600

    pass redis environment var to user's notebook

--- berdlhub/config/environment.py
+++ berdlhub/config/environment.py
@@ -22,6 +22,8 @@ def configure_environment(c):
         "MINIO_ENDPOINT_URL": os.environ["MINIO_ENDPOINT_URL"],
         "MINIO_SECURE": os.environ.get("MINIO_SECURE_FLAG", "True"),
         "DATALAKE_MCP_SERVER_URL": os.environ["DATALAKE_MCP_SERVER_URL"],
+        "BERDL_REDIS_HOST": os.environ["BERDL_REDIS_HOST"],
+        "BERDL_REDIS_PORT": os.environ["BERDL_REDIS_PORT"],
     }
 
     # Jupyter Docker Stacks configuration
