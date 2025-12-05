e43c34ac5d5e2082829ff142e16d1295b4590293
Tianhao-Gu <tgu@anl.gov>
2025-12-05 14:37:25 -0600

    use username for spawner

--- berdlhub/config/spawner.py
+++ berdlhub/config/spawner.py
@@ -27,9 +27,11 @@ def configure_spawner(c):
     c.KubeSpawner.delete_stopped_pods = True
 
     # Notebook settings
-    c.KubeSpawner.notebook_dir = "/home/{username}"
+    # IMPORTANT: working_dir is expanded via _expand_user_properties(), so use {unescaped_username}
+    # to avoid hash suffixes for usernames with special characters
+    c.KubeSpawner.notebook_dir = "/home/{unescaped_username}"
     c.KubeSpawner.cmd = ["start-notebook.sh"]
-    c.KubeSpawner.working_dir = "/home/{username}"
+    c.KubeSpawner.working_dir = "/home/{unescaped_username}"
     c.KubeSpawner.args = [
         "--ServerApp.default_url=/lab",
     ]
