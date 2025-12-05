b21eedd0614d0c8a59a9eab5d22304c9f53d500f
Tianhao Gu <Tianhao-Gu@users.noreply.github.com>
2025-12-05 16:03:13 -0600

    using unescaped_username (#31)
    * using unescaped_username
    
    * fix tests
    
    * use username for spawner
    
    * use username for spawner
    
    * use username for spawner
    
    * use username for spawner
    
    * update environment

--- berdlhub/config/environment.py
+++ berdlhub/config/environment.py
@@ -2,6 +2,26 @@
 
 import os
 
+from .spark_connect_service import sanitize_k8s_name
+
+
+def get_spark_connect_url(spawner):
+    """
+    Generate the Spark Connect URL for the user's notebook.
+
+    This callable is used by KubeSpawner to dynamically generate the URL
+    based on the sanitized username (matching the Service name).
+
+    Args:
+        spawner: KubeSpawner instance
+
+    Returns:
+        Spark Connect URL string (sc://jupyter-{sanitized-username}.{namespace}:15002)
+    """
+    sanitized_username = sanitize_k8s_name(spawner.user.name)
+    namespace = spawner.namespace
+    return f"sc://jupyter-{sanitized_username}.{namespace}:15002"
+
 
 def configure_environment(c):
     """Configure environment variables for notebook pods."""
@@ -24,8 +44,13 @@ def configure_environment(c):
         "DATALAKE_MCP_SERVER_URL": os.environ["DATALAKE_MCP_SERVER_URL"],
         "BERDL_REDIS_HOST": os.environ["BERDL_REDIS_HOST"],
         "BERDL_REDIS_PORT": os.environ["BERDL_REDIS_PORT"],
+        # Spark Connect URL (uses callable to generate from sanitized username)
+        # This must match the Service name created in spark_connect_service.py
+        "SPARK_CONNECT_URL": get_spark_connect_url,
     }
 
     # Jupyter Docker Stacks configuration
     # https://jupyter-docker-stacks.readthedocs.io/en/latest/using/common.html
-    c.KubeSpawner.environment.update({"NB_USER": "{username}", "CHOWN_HOME": "yes", "GEN_CERT": "yes"})
+    # IMPORTANT: Use {unescaped_username} because c.KubeSpawner.environment goes through
+    # _expand_user_properties(), not template_namespace()
+    c.KubeSpawner.environment.update({"NB_USER": "{unescaped_username}", "CHOWN_HOME": "yes", "GEN_CERT": "yes"})
