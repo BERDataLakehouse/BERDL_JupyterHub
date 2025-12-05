a3b899f75feeb25ee77d9028822911a2c078ba46
Tianhao-Gu <tgu@anl.gov>
2025-12-05 16:01:51 -0600

    update environment

--- berdlhub/config/environment.py
+++ berdlhub/config/environment.py
@@ -2,6 +2,26 @@
 
 import os
 
+from .spark_connect_service import sanitize_k8s_name
+
+
+def get_spark_connect_url(spawner):
+    """
+    Generate the Spark Connect URL for the user's notebook.
+
+    This callable is used by KubeSpawner to dynamically generate the URL
+    based on the sanitized username (matching the Service name).
+
+    Args:
+        spawner: KubeSpawner instance
+
+    Returns:
+        Spark Connect URL string (sc://jupyter-{sanitized-username}.{namespace}:15002)
+    """
+    sanitized_username = sanitize_k8s_name(spawner.user.name)
+    namespace = spawner.namespace
+    return f"sc://jupyter-{sanitized_username}.{namespace}:15002"
+
 
 def configure_environment(c):
     """Configure environment variables for notebook pods."""
@@ -11,7 +31,7 @@ def configure_environment(c):
         # KBase integration
         "KBASE_ORIGIN": os.environ["KBASE_ORIGIN"],
         # Spark configuration
-        "SPARK_JOB_LOG_DIR_CATEGORY": "{unescaped_username}",
+        "SPARK_JOB_LOG_DIR_CATEGORY": "{username}",
         "CDM_TASK_SERVICE_URL": os.environ["CDM_TASK_SERVICE_URL"],
         "SPARK_CLUSTER_MANAGER_API_URL": os.environ["SPARK_CLUSTER_MANAGER_API_URL"],
         # Hive metastore
@@ -24,6 +44,9 @@ def configure_environment(c):
         "DATALAKE_MCP_SERVER_URL": os.environ["DATALAKE_MCP_SERVER_URL"],
         "BERDL_REDIS_HOST": os.environ["BERDL_REDIS_HOST"],
         "BERDL_REDIS_PORT": os.environ["BERDL_REDIS_PORT"],
+        # Spark Connect URL (uses callable to generate from sanitized username)
+        # This must match the Service name created in spark_connect_service.py
+        "SPARK_CONNECT_URL": get_spark_connect_url,
     }
 
     # Jupyter Docker Stacks configuration
